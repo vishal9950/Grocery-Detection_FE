830e9783158cb6fb18c3b6d89c7df04fecf7c0fd
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-04-08 20:21:52 +0530

    Added input to upload file

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  fileInput = (event) => {
+    console.log(event.target.files[0]);
+    const data = new FormData();
+    data.append('file', event.target.files[0]);
+    this.setState({
+      data,
+    });
+  }
+
+  saveFile = () => {
+    console.log('called');
+    axios.post('/evaluate', this.state.data).then((response) => {
+      console.log(response);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App-main">
+        <input type="file" onChange={this.fileInput} />
+        <button onClick={() => this.saveFile()}>Upload</button>
+      </div>
+    );
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root'),
+  );
+};
+
+
+export default render;
